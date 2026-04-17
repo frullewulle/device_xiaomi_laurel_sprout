cc5221b3a56587a4efeaa296b982c4ca3245e3b5
frullewulle <dirk.lemaire@gmail.com>
2026-04-17 21:41:46 +0200

    Update lineage_laurel_sprout.mk

--- lineage_laurel_sprout.mk
+++ lineage_laurel_sprout.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common BeastROMs stuff.
+# Inherit some common LineageROMs stuff.
 $(call inherit-product, vendor/lineage/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 

514c031d41740f5d2fda39c97ec745a7bcf03e60
frullewulle <dirk.lemaire@gmail.com>
2026-04-17 21:35:29 +0200

    Update lineage_laurel_sprout.mk

--- lineage_laurel_sprout.mk
+++ lineage_laurel_sprout.mk
@@ -20,17 +20,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common BeastROMs stuff.
-$(call inherit-product, vendor/beast/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := beast_laurel_sprout
+PRODUCT_NAME := lineage_laurel_sprout
 PRODUCT_DEVICE := laurel_sprout
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI A3
 PRODUCT_MANUFACTURER := Xiaomi
 
-BEAST_BUILD_TYPE := OFFICIAL
+LINEAGE_BUILD_TYPE := OFFICIAL
 
 BUILD_FINGERPRINT := "Xiaomi/laurel_sprout_eea/laurel_sprout:9/PKQ1.190416.001/V10.3.7.0.PFQEUXM:user/release-keys"
 
